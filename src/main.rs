ea519b579a3d835665f33e28f3c05138920eed76
leacock <dave.leacock@gmail.com>
2026-05-31 17:40:22 -0700

    rearrange main

--- src/main.rs
+++ src/main.rs
@@ -13,36 +13,32 @@ mod projectile;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        // Startup
         .add_systems(Startup, map::setup_world)
         .add_systems(Startup, debug::setup)
-
+        // Player
         .add_systems(Startup, player::setup)
         .add_systems(Update, player::move_player)
         .add_systems(Update, player::fire_weapon)
         .add_systems(Update, player::update_camera)
-
+        .insert_resource(PlayerStats { gems: 0 })
+        // Enemy
         .add_systems(Startup, enemy::spawn_enemies)
         .add_systems(Update, enemy::move_enemies)
         .add_systems(Update, enemy::detect_collisions)
-
+        // Projectile
         .add_systems(Update, projectile::move_projectiles)
         .add_systems(Update, projectile::detect_projectile_collisions)
-
+        // Pickups
         .add_systems(Update, pickup::detect_collisions)
         .add_systems(Update, pickup::update_pickups)
-
+        .add_observer(pickup::on_enemy_killed)
+        // Debug
         .add_systems(Update, debug::debug_inputs)
         .add_systems(Update, debug::draw_debug_visuals)
         .add_systems(Update, debug::draw_debug_data)
-
-        .add_observer(pickup::on_enemy_killed)
-
         .insert_resource(DebugConfig {
             visuals_enabled: false,
         })
-        .insert_resource(PlayerStats {
-            gems: 0,
-        })
-
         .run();
 }
